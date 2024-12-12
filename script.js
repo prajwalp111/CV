554a4d953c4d1c295defb7a6fe62f43ec40efb86
MannanFaiz <mannanfaiz@gmail.com>
2024-12-12 15:25:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("header nav ul li a").on("click", function () {
+        $("header nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+});
